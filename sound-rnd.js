8b6f2e8a2bb94dfeb80a0d7252ee7e091b9de8d9
ObiDriftKenobi <gdpancake5@gmail.com>
2020-07-16 22:03:20 +0200

    Obeyed Colon's instructions

--- sound-rnd.js
+++ sound-rnd.js
@@ -1,6 +1,8 @@
 var fs = require("fs")
 const path = require('path');
 
+const gdPath = fs.readFileSync('directory.txt', 'utf8')
+
 Array.prototype.shuffle = function() {
     let length = this.length; let unshuffled = this; let shuffled = [];
     while (shuffled.length !== length) {
@@ -10,24 +12,24 @@ Array.prototype.shuffle = function() {
     return shuffled;
 }  // I definitely didn't copy that from your code, Craig
 
-var gdFiles = fs.readdirSync("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash\\Resources");
+var gdFiles = fs.readdirSync(gdPath);
 var mp3s = gdFiles.filter((file) => file.endsWith(".mp3"));
 var oggs = gdFiles.filter((file) => file.endsWith(".ogg"));
 var sounds = mp3s.concat(oggs)
 
 // Prefix every file with #, to avoid errors with existing files
 sounds.forEach(sound => {  
-    fs.renameSync(path.join("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash\\Resources",sound),path.join("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash\\Resources","#" + sound))
+    fs.renameSync(path.join(gdPath,sound),path.join(gdPath,"#" + sound))
 });
 
 var mp3dest = [...mp3s].shuffle() 
 for (i=0; i < mp3s.length; i++) {
-    fs.renameSync(path.join("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash\\Resources","#" + mp3s[i]),path.join("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash\\Resources",mp3dest[i]))
+    fs.renameSync(path.join(gdPath,"#" + mp3s[i]),path.join(gdPath,mp3dest[i]))
 }
 
 var oggDest = [...oggs].shuffle()
 for (i=0; i < oggs.length; i++) {
-    fs.renameSync(path.join("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash\\Resources","#" + oggs[i]),path.join("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Geometry Dash\\Resources",oggDest[i]))
+    fs.renameSync(path.join(gdPath,"#" + oggs[i]),path.join(gdPath,oggDest[i]))
 }
 
 console.log("dunsparce.")
